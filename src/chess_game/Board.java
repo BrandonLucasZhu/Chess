355aee95f5961e113240cc6b538b04e7cb6d40ed
Brandon Lucas Zhu <Brandonlucaszhu@gmail.com>
2017-12-09 15:40:26 -0500

    Prepared classes

--- src/chess_game/Board.java
+++ src/chess_game/Board.java
@@ -1,5 +1,21 @@
 package chess_game;
 
 public class Board {
+	
+	private Point[][] grid = new Point[8][8];
+	
+	public Board() {
+		for (int i = 0; i < grid.length;i++) {
+			for (int j=0; j < grid[i].length;j++) {
+					this.grid[i][j] = new Point(i,j);//Create board
+			}
+		}
+	}
+	
+	public Point get_point(int x, int y) {
+	        return grid[x][y];
+	}
+	
+	
 
 }
--- src/chess_game/Point.java
+++ src/chess_game/Point.java
@@ -2,4 +2,31 @@ package chess_game;
 
 public class Point {
 
+	private int x, y;
+	 
+	 
+	public Point(int i, int j) {
+		x = i; 
+		y = j;
+		
+	}
+	 
+	public	int getX() {
+		return x; 
+	}
+	
+	public int getY(){
+		return y;
+	}
+	 
+	public void setX(int i) {
+		x=i; 
+	}
+	 
+	public void setY(int j) {
+		y=j; 
+	}
+	 
+	
+	 
 }
